62ba5c398b669a6aa4c8b9123fe6c08d7391fcaa
Joao Pedro da Cunha <joaopedrocunhacontato@gmail.com>
2025-11-16 00:16:27 -0300

    feat: Modificacao de classe Carta para modificar Jogador

--- src/Carta.java
+++ src/Carta.java
@@ -1,3 +1,13 @@
 public abstract class Carta {
-    public abstract void executarAcao();
+
+    public abstract void executarAcao(Jogador jogador); // Executa a acao da carta
+
+    public abstract String getNome(); // Retorna o nome da carta
+
+    // Verifica se a carta pode bloquear uma acao
+
+    public boolean podeBloquear(String acao) {
+        return false;
+    }
+
 }
